0e24930c07bc4d29071968355ca9a5f386420efb
Bsrkkyc <busrikky.c.kopaska.17@gmail.com>
2022-01-18 18:48:33 +0700

    UAS-Busriki

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { DivisionComponent } from './division/division.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { TambahDataComponent } from './tambah-data/tambah-data.component';
+import { EditDataComponent } from './edit-data/edit-data.component';
+import { HapusComponent } from './hapus/hapus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    DivisionComponent,
+    DashboardComponent,
+    AppFooterComponent,
+    TambahDataComponent,
+    EditDataComponent,
+    HapusComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hapus/hapus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hapus',
+  templateUrl: './hapus.component.html',
+  styleUrls: ['./hapus.component.css']
+})
+export class HapusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
